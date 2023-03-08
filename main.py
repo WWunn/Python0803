8fc61673b45cde2521c6680f40ff4824ca26c9c4
--set <--set>
2023-03-08 15:55:03 +0100

    st

---
+++ main.py
@@ -0,0 +1,44 @@
+a="napis \n drugi napis"
+print(a)
+b=5
+c=5.5
+print(b,c)
+d=5+3j
+print(d)
+e=c+d
+print(e)
+f=c//b
+print(f)
+g=c%b
+print(g)
+h=b**b
+print(h)
+i=b**1/2
+j=pow(b,1/2)
+print(j)
+print(str(b)+'= b + 2')
+print(b)
+b+=2
+print(b)
+listy=[4,5,6,5.6]
+print(listy)
+listy.append(4)
+print(listy)
+hemoglobina=[3,24,6,8,69,69,241]
+#dodac element na wybrane miejsce
+listy.insert(3,4)
+#dodac kilka elementow
+listy.extend(hemoglobina)
+print(listy)
+#usunac element o danej pozycji
+listy.pop(1)
+print(listy)
+#usunac element o danej wartosci
+listy.remove(69)
+print(listy)
+#sortowanie
+listy.sort()
+print(listy)
+#odwracanie
+listy.reverse()
+print(listy)
